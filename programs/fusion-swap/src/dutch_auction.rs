f8de2eb22b895fc281f26d7ab63ca3568147bf76
Denis <zzomrot.ne@gmail.com>
2025-02-13 12:21:42 +0000

    Extract finish_time to the variable to avoid multiple casts

--- programs/fusion-swap/src/dutch_auction.rs
+++ programs/fusion-swap/src/dutch_auction.rs
@@ -21,7 +21,8 @@ pub fn calculate_rate_bump(timestamp: u64, data: &DutchAuctionData) -> u64 {
     if timestamp <= data.auction_start_time as u64 {
         return data.initial_rate_bump as u64;
     }
-    if timestamp >= data.auction_finish_time as u64 {
+    let auction_finish_time = data.auction_finish_time as u64;
+    if timestamp >= auction_finish_time {
         return 0;
     }
 
@@ -40,6 +41,6 @@ pub fn calculate_rate_bump(timestamp: u64, data: &DutchAuctionData) -> u64 {
         current_rate_bump = next_rate_bump;
         current_point_time = next_point_time;
     }
-    (data.auction_finish_time as u64 - timestamp) * current_rate_bump
-        / (data.auction_finish_time as u64 - current_point_time)
+    (auction_finish_time - timestamp) * current_rate_bump
+        / (auction_finish_time - current_point_time)
 }
