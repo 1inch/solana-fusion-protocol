9de291663f8d1af968f49f762f52ab3f063a4295
Denis <zzomrot.ne@gmail.com>
2025-02-13 21:15:47 +0000

    Move auction constant from common workspace

--- common/src/constants.rs
+++ common/src/constants.rs
@@ -1,7 +1,5 @@
 /// Discriminator size in bytes
 pub const DISCRIMINATOR: usize = 8;
-/// Max amount of points in the dutch auction
-pub const MAX_AUCTION_POINTS: usize = 5;
 
 pub const BASE_1E2: u64 = 100;
 pub const BASE_1E5: u64 = 100_000;
