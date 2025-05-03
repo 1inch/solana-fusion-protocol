98c78f2011559bbcb85ea6e5d72c0fa87cb3f850
Denis <zzomrot.ne@gmail.com>
2025-05-03 19:47:36 +0100

    Remove fusion-swap-native program

--- native-programs/fusion-swap-native/src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod entrypoint;
-pub mod error;
-pub mod processor;
-pub mod types;
-pub mod utils;
-pub mod validation_helpers;
-
-solana_program::declare_id!("4NoMYjvQeLuNqvRhfBM5gzc54KVEDTzGyAwf8CFdYhJs");
